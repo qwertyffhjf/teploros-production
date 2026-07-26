3bc0acea7b3941580dc23983eb5452a8077535e7
qwertyffhjf <fred8834@gmail.com>
2026-07-26 13:04:22 +0300

    Update service worker cache version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // teploros Service Worker v3 — устойчивое кеширование
-const APP_CACHE = 'teploros-app-v20260726c';
+const APP_CACHE = 'teploros-app-v20260727a';
 const CDN_CACHE = 'teploros-cdn-v1';
 
 const ASSETS = [
