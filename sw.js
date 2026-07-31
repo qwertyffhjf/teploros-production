5c30e4421dbb6aeee0961dc7fb7dd724ea66ac9a
qwertyffhjf <fred8834@gmail.com>
2026-07-31 10:08:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // teploros Service Worker v3 — устойчивое кеширование
-const APP_CACHE = 'teploros-app-v20260731';
+const APP_CACHE = 'teploros-app-v20260802';
 const CDN_CACHE = 'teploros-cdn-v1';
 
 const ASSETS = [
